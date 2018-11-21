a3139d713ffdd3555996f14f96e3d0336ae407f9
Michal <jenicek.michal@gmail.com>
2018-11-21 14:17:17 +0100

    Just formatting.

--- app/src/main/kotlin/cz/koto/kotiheating/model/entity/heatingStatus.kt
+++ app/src/main/kotlin/cz/koto/kotiheating/model/entity/heatingStatus.kt
@@ -5,19 +5,19 @@ import com.google.gson.annotations.SerializedName
 import java.util.Date
 
 data class HeatingStatusResult(
-		@SerializedName("result") val heatingDeviceStatus: HeatingDeviceStatus
+	@SerializedName("result") val heatingDeviceStatus: HeatingDeviceStatus
 )
 
 @Entity(tableName = "deviceStatus", primaryKeys = ["deviceId"])
 @JvmSuppressWildcards
 data class HeatingDeviceStatus(
-		@SerializedName("timestamp") val timestamp: Date,
-		@SerializedName("temperature") val temperature: Int,
-		@SerializedName("heatingMode") val deviceMode: Int,
-		@SerializedName("day") val deviceDay: String,
-		@SerializedName("minute") val deviceMinute: Int,
-		@SerializedName("hour") val deviceHour: Int,
+	@SerializedName("timestamp") val timestamp: Date,
+	@SerializedName("temperature") val temperature: Int,
+	@SerializedName("heatingMode") val deviceMode: Int,
+	@SerializedName("day") val deviceDay: String,
+	@SerializedName("minute") val deviceMinute: Int,
+	@SerializedName("hour") val deviceHour: Int,
 	@SerializedName("timetableDevice") val timetableDevice: MutableList<MutableList<Int>>,
 	@SerializedName("timetableServer") val timetableServer: MutableList<MutableList<Int>>,
-		@SerializedName("heatingId") val deviceId: String
+	@SerializedName("heatingId") val deviceId: String
 )
